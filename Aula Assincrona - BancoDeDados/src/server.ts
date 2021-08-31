2661a37d38a10b52358599e7cec58d30263908cc
lecesion <leticia.cesion@outlook.com>
2021-08-30 23:21:14 -0300

    express.json

--- Aula Assincrona - BancoDeDados/src/server.ts
+++ Aula Assincrona - BancoDeDados/src/server.ts
@@ -4,9 +4,9 @@ import routes from './routes';
 
 const app = express();
 
-app.use(routes);
-
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log('Servidor inicializado com sucesso na porta 3000.'));
 
